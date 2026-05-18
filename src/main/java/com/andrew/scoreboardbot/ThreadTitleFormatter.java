ca2384c94e4b8709bf41248268a9424517b4ebef
Andrew <tornadodude98@gmail.com>
2026-05-17 21:23:12 -0500

    Updated thread title logic to account for doubleheaders.

--- src/main/java/com/andrew/scoreboardbot/ThreadTitleFormatter.java
+++ src/main/java/com/andrew/scoreboardbot/ThreadTitleFormatter.java
@@ -20,7 +20,7 @@ public class ThreadTitleFormatter
             case "D" -> game.away().teamInfo().league().equals(League.AMERICAN) ? "ALDS GM " + game.getSeriesGameNumber() + " - " + away + " @ " + home : "NLDS GM " + game.getSeriesGameNumber() + " - " + away + " @ " + home;
             case "L" -> game.away().teamInfo().league().equals(League.AMERICAN) ? "ALCS GM " + game.getSeriesGameNumber() + " - " + away + " @ " + home : "NLCS GM " + game.getSeriesGameNumber() + " - " + away + " @ " + home;
             case "W" -> "WS GM " + game.getSeriesGameNumber() + " - " + away + " @ " + home;
-            default -> away + " @ " + home + " - " + game.gameDate().withZoneSameInstant(ZoneId.of("America/New_York")).format(DATE_FORMAT);
+            default -> game.isDoubleheader() ? away + " @ " + home + " GM " + game.gameNumber() + " - " + game.gameDate().withZoneSameInstant(ZoneId.of("America/New_York")).format(DATE_FORMAT) : away + " @ " + home + " - " + game.gameDate().withZoneSameInstant(ZoneId.of("America/New_York")).format(DATE_FORMAT);
         };
     }
 }
